e9d78090b32762f1d9a95f5795c5955a542a3a55
Julian Feliciano <julsfelic@gmail.com>
2016-01-11 21:27:51 -0700

    Build initial framework for District class

---
+++ test/district_test.rb
@@ -0,0 +1,21 @@
+require "district"
+
+class DistrictTest < Minitest::Test
+  def test_can_create_instances
+    d = District.new(name: "ACADEMY 20")
+
+    assert_instance_of District, d
+  end
+
+  def test_can_take_a_has_with_a_name_key
+    d = District.new(name: "ACADEMY 20")
+
+    assert_equal "ACADEMY 20", d.name
+  end
+
+  def test_upcases_the_name_upon_creation
+    d = District.new(name: "academy 20")
+
+    assert_equal "ACADEMY 20", d.name
+  end
+end
