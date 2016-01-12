64d8360c03a0d21341f129489dfd48eadd553c9d
Nicholas Dorans <Nicky_Bobby@icloud.com>
2016-01-12 10:40:13 -0700

    new code but still not working

--- lib/enrollment_repository.rb
+++ lib/enrollment_repository.rb
@@ -1,39 +1,40 @@
 require 'pry'
 require 'csv'
+require 'bigdecimal'
 
 class EnrollmentRepository
-
+  attr_reader :years, :kindergarten
   def initialize
     @kindergarten = Hash.new
-
+    @years = Hash.new
   end
 
   def load_data(enrollment_data)
     kindergarten_csv = enrollment_data[:enrollment][:kindergarten]
-    contents = CSV.open kindergarten_csv, headers: true, header_converters: :symbols
-    # parse_for_data(contents)
-    contents.each do |row|
-      binding.pry
-    #   district = row[:location]
-    #   year = row[:timeframe]
-    #   participation = row[:data]
-      # @kindergarten[:district] = Hash.new
-    end
-
+    contents = CSV.open kindergarten_csv, headers: true, header_converters: :symbol
+    parse_for_data(contents)
   end
 
-  # def parse_for_data(contents)
-  #   contents.each do |row|
-  #     district = row[:location]
-  #     year = row[:timeframe]
-  #     participation = row[:data]
-  #     @kindergarten[:district] = Hash.new
-  #     binding.pry
-  #   end
-  # end
+  def parse_for_data(contents)
+    contents.each do |row|
+      district = row[:location]
+      year = row[:timeframe]
+      participation = row[:data][0..4].to_f
+      # participation = BigDecimal.new(participation)
+      years_hash(participation, year)
+      # @years[year.to_sym] = participation
+      @kindergarten[district.to_sym] = @years
+    binding.pry
+    end
+  end
 
-  def find_by_name
+  def years_hash(participation, year)
+    @years[year] = participation
+  end
 
+  def find_by_name(district)
+    e = @kindergarten[district.to_sym]
+    p e
   end
 
 end
