6b6501d41a02df41b1ccb7174bd9f992573d76e2
Julian Feliciano <julsfelic@gmail.com>
2016-01-14 11:14:15 -0700

    Add participation test against other district

--- test/headcount_analyst_test.rb
+++ test/headcount_analyst_test.rb
@@ -14,14 +14,6 @@ class HeadcountAnalystTest < Minitest::Test
     assert_instance_of HeadcountAnalyst, ha
   end
 
-  def test_can_get_kindergarten_participation_against_state_average
-    ha = HeadcountAnalyst.new(dr)
-    rate = ha.kindergarten_participation_rate_variation("ACADEMY 20",
-                                                        against: "COLORADO")
-
-    assert_equal 0.766, rate
-  end
-
   def test_can_get_both_districts
     ha = HeadcountAnalyst.new(dr)
     ha.load_district_repo_data
@@ -31,5 +23,22 @@ class HeadcountAnalystTest < Minitest::Test
     assert_equal "ACADEMY 20", districts.first.name
     assert_instance_of District, districts.last
     assert_equal "COLORADO", districts.last.name
+  end
+
+  def test_can_get_kindergarten_participation_against_state_average
+    skip
+    ha = HeadcountAnalyst.new(dr)
+    rate = ha.kindergarten_participation_rate_variation("ACADEMY 20",
+                                                        against: "COLORADO")
+
+    assert_equal 0.766, rate
+  end
+
+  def test_can_get_kindergarten_participation_against_another_district
+    ha = HeadcountAnalyst.new(dr)
+    rate = ha.kindergarten_participation_rate_variation("ACADEMY 20",
+                                                        against: "YUMA SCHOOL DISTRICT")
+
+    assert_equal 1.234, rate
   end
 end
