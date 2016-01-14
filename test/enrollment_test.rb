71e1ff355bea22c75d0847a17a1d87dcea08a532
Julian Feliciano <julsfelic@gmail.com>
2016-01-14 10:41:02 -0700

    Implement hound changes

--- test/enrollment_test.rb
+++ test/enrollment_test.rb
@@ -4,32 +4,38 @@ require "enrollment"
 class EnrollmentTest < Minitest::Test
 
   def test_can_be_instantiated_on
-    e = Enrollment.new({:name => "ACADEMY 20",
-                        :kindergarten_participation => {
+    e = Enrollment.new(name: "ACADEMY 20",
+                        kindergarten_participation:
+                        {
                           2010 => 0.3915,
                           2011 => 0.35356,
                           2012 => 0.2677
-    }})
+                        }
+                       )
     assert_instance_of Enrollment, e
   end
 
   def test_can_take_an_argument_with_a_name_in_it
-    e = Enrollment.new({:name => "Colorado",
-                        :kindergarten_participation => {
+    e = Enrollment.new(name: "Colorado",
+                        kindergarten_participation:
+                        {
                           2010 => 0.3915,
                           2011 => 0.35356,
                           2012 => 0.2677
-    }})
+                        }
+                       )
     assert_equal "COLORADO", e.name
   end
 
   def test_will_return_list_of_kindergarten_participation_by_year
-    e = Enrollment.new({:name => "ACADEMY 20",
-                        :kindergarten_participation => {
+    e = Enrollment.new(name: "ACADEMY 20",
+                        kindergarten_participation:
+                        {
                           2010 => 0.3915,
                           2011 => 0.35356,
                           2012 => 0.2677
-    }})
+                        }
+                       )
     expected = { 2010 => 0.392, 2011 => 0.354, 2012 => 0.268 }
 
     assert_instance_of Enrollment, e
@@ -37,22 +43,26 @@ class EnrollmentTest < Minitest::Test
   end
 
   def test_will_return_participation_of_specific_year
-    e = Enrollment.new({:name => "ACADEMY 20",
-                        :kindergarten_participation => {
+    e = Enrollment.new(name: "ACADEMY 20",
+                        kindergarten_participation:
+                        {
                           2010 => 0.3915,
                           2011 => 0.35356,
                           2012 => 0.2677
-    }})
+                        }
+                       )
     assert_equal 0.392, e.kindergarten_participation_in_year(2010)
   end
 
   def test_sanitizes_participation
-    e = Enrollment.new({:name => "ACADEMY 20",
-                        :kindergarten_participation => {
+    e = Enrollment.new(name: "ACADEMY 20",
+                        kindergarten_participation:
+                        {
                           2010 => 0.3915,
                           2011 => 0.35356,
                           2012 => 0.2677
-    }})
+                        }
+                       )
     sanitized = e.sanitize({ 2010 => 0.3915 })
     expected = { 2010 => 0.392 }
 
@@ -60,12 +70,14 @@ class EnrollmentTest < Minitest::Test
   end
 
   def test_gets_average_participation_for_all_years
-    e = Enrollment.new({:name => "ACADEMY 20",
-                        :kindergarten_participation => {
-                          2010 => 0.3915, # .392
-                          2011 => 0.35356,# .354
-                          2012 => 0.2677  # .268
-    }})
+    e = Enrollment.new(name: "ACADEMY 20",
+                        kindergarten_participation:
+                        {
+                          2010 => 0.3915,
+                          2011 => 0.35356,
+                          2012 => 0.2677
+                        }
+                       )
     average = e.get_participation_average
 
     assert_equal 0.338, average
--- test/headcount_analyst_test.rb
+++ test/headcount_analyst_test.rb
@@ -16,7 +16,8 @@ class HeadcountAnalystTest < Minitest::Test
 
   def test_can_get_kindergarten_participation_against_state_average
     ha = HeadcountAnalyst.new(dr)
-    rate = ha.kindergarten_participation_rate_variation('ACADEMY 20', against: 'COLORADO')
+    rate = ha.kindergarten_participation_rate_variation("ACADEMY 20",
+                                                         against: "COLORADO")
 
     assert_equal 0.766, rate
   end
@@ -24,7 +25,7 @@ class HeadcountAnalystTest < Minitest::Test
   def test_can_get_both_districts
     ha = HeadcountAnalyst.new(dr)
     ha.load_district_repo_data
-    districts = ha.grab_districts('ACADEMY 20', 'COLORADO')
+    districts = ha.grab_districts("ACADEMY 20", "COLORADO")
 
     assert_instance_of District, districts.first
     assert_equal 'ACADEMY 20', districts.first.name
