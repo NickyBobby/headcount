9fa00a1805fe9c61c2dfb944538d13772a8369bf
Julian Feliciano <julsfelic@gmail.com>
2016-01-14 09:38:17 -0700

    can load district repo data and grab districts

--- lib/headcount_analyst.rb
+++ lib/headcount_analyst.rb
@@ -1,4 +1,4 @@
-
+require_relative "district_repository"
 
 class HeadcountAnalyst
   attr_reader :district_repo
@@ -7,6 +7,25 @@ class HeadcountAnalyst
     @district_repo = district_repo
   end
 
-  
+  def load_district_repo_data
+    district_repo.load_data({
+      enrollment: {
+        kindergarten: "./data/Kindergartners in full-day program.csv"
+      }
+    })
+  end
 
+  def grab_districts(district, district_against)
+    d1 = district_repo.find_by_name(district)
+    d2 = district_repo.find_by_name(district_against)
+    [d1, d2]
+  end
+
+  def kindergarten_participation_rate_variation(district, compared_district)
+    load_district_repo_data
+    districts = grab_districts(district, compared_district[:against])
+    # Grab the participation rates for all years (both districts)
+    # Divide district average / compared district average
+
+  end
 end
