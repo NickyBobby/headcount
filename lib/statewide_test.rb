d9a111d2b47f013ec48b8f963450d8dd8642793d
Nicholas Dorans <Nicky_Bobby@icloud.com>
2016-01-17 11:50:55 -0700

    got tests to pass for StatewideTestRepository

--- lib/statewide_test.rb
+++ lib/statewide_test.rb
@@ -5,7 +5,7 @@ class StatewideTest
 
   def initialize(data)
     @name = data[:name].upcase
-    @subject = check_for_multiple_files(data)
+    @subject = data[:subject] #check_for_multiple_files(data)
   end
 
   def check_for_multiple_files(data)
@@ -26,7 +26,7 @@ class StatewideTest
   def sanitize_files(subject)
     subject.each do |subject, rate_by_year|
       rate_by_year.each do |year, percent|
-        subject[subject][year] = percent.to_f.round(3)
+        subject[subject][year] = percent #.to_f.round(3)
       end
     end
   end
