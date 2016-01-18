be573f5c3704a73f2d048629a77be4fffd0f0211
Julian Feliciano <julsfelic@gmail.com>
2016-01-18 13:12:10 -0700

    Economic profile can calculate median household income in year and raise UnknownDataError if not given a valid year

--- lib/economic_profile.rb
+++ lib/economic_profile.rb
@@ -1,3 +1,6 @@
+require "pry"
+require_relative "unknown_data_error"
+
 class EconomicProfile
   attr_reader :name, :median_household_income, :children_in_poverty,
               :free_or_reduced_price_lunch, :title_i
@@ -9,4 +12,23 @@ class EconomicProfile
     @free_or_reduced_price_lunch = data[:free_or_reduced_price_lunch]
     @title_i                     = data[:title_i]
   end
+
+  def median_household_income_in_year(year)
+    raise_for_unknown_year(year, median_household_income.keys)
+    incomes = []
+    median_household_income.each_key do |range|
+      next unless year.between?(range.first, range.last)
+      incomes << median_household_income[range]
+    end
+    incomes.inject(0, :+) / incomes.count
+  end
+
+  private
+
+    def raise_for_unknown_year(year, ranges)
+      exists = ranges.any? do |range|
+        year.between?(range.first, range.last)
+      end
+      raise UnknownDataError unless exists
+    end
 end
--- test/economic_profile_test.rb
+++ test/economic_profile_test.rb
@@ -27,4 +27,29 @@ class EconomicProfileTest < Minitest::Test
     ep = EconomicProfile.new(data)
     assert_equal "ACADEMY 20", ep.name
   end
+
+  def test_returns_median_household_income_in_year
+    data = {:median_household_income => {[2005, 2009] => 50000, [2008, 2014] => 60000},
+            :children_in_poverty => {2012 => 0.1845},
+            :free_or_reduced_price_lunch => {2014 => {:percentage => 0.023, :total => 100}},
+            :title_i => {2015 => 0.543},
+            :name => "academy 20"
+    }
+    ep = EconomicProfile.new(data)
+    assert_equal 50000, ep.median_household_income_in_year(2005)
+    assert_equal 55000, ep.median_household_income_in_year(2009)
+  end
+
+  def test_raises_unknown_data_error_for_unknown_year
+    data = {:median_household_income => {[2005, 2009] => 50000, [2008, 2014] => 60000},
+            :children_in_poverty => {2012 => 0.1845},
+            :free_or_reduced_price_lunch => {2014 => {:percentage => 0.023, :total => 100}},
+            :title_i => {2015 => 0.543},
+            :name => "academy 20"
+    }
+    ep = EconomicProfile.new(data)
+    assert_raises UnknownDataError do
+      ep.median_household_income_in_year(1999)
+    end
+  end
 end
