c9df01b6c64d8548e157a5139ac420669f237e88
Julian Feliciano <julsfelic@gmail.com>
2016-01-18 13:34:19 -0700

    create a economic data method helper for testing

--- test/economic_profile_test.rb
+++ test/economic_profile_test.rb
@@ -3,64 +3,34 @@ require "economic_profile"
 
 class EconomicProfileTest < Minitest::Test
   def test_takes_in_data_when_created
-    data = {:median_household_income => {[2005, 2009] => 50000, [2008, 2014] => 60000},
-            :children_in_poverty => {2012 => 0.1845},
-            :free_or_reduced_price_lunch => {2014 => {:percentage => 0.023, :total => 100}},
-            :title_i => {2015 => 0.543},
-            :name => "ACADEMY 20"
-    }
-    ep = EconomicProfile.new(data)
+    ep = EconomicProfile.new(economic_data)
     assert_equal "ACADEMY 20", ep.name
-    assert_equal data[:median_household_income], ep.median_household_income
-    assert_equal data[:children_in_poverty], ep.children_in_poverty
-    assert_equal data[:free_or_reduced_price_lunch], ep.free_or_reduced_price_lunch
-    assert_equal data[:title_i], ep.title_i
+    assert_equal economic_data[:median_household_income], ep.median_household_income
+    assert_equal economic_data[:children_in_poverty], ep.children_in_poverty
+    assert_equal economic_data[:free_or_reduced_price_lunch], ep.free_or_reduced_price_lunch
+    assert_equal economic_data[:title_i], ep.title_i
   end
 
   def test_upcases_name_when_created
-    data = {:median_household_income => {[2005, 2009] => 50000, [2008, 2014] => 60000},
-            :children_in_poverty => {2012 => 0.1845},
-            :free_or_reduced_price_lunch => {2014 => {:percentage => 0.023, :total => 100}},
-            :title_i => {2015 => 0.543},
-            :name => "academy 20"
-    }
-    ep = EconomicProfile.new(data)
+    ep = EconomicProfile.new(economic_data)
     assert_equal "ACADEMY 20", ep.name
   end
 
   def test_returns_median_household_income_in_year
-    data = {:median_household_income => {[2005, 2009] => 50000, [2008, 2014] => 60000},
-            :children_in_poverty => {2012 => 0.1845},
-            :free_or_reduced_price_lunch => {2014 => {:percentage => 0.023, :total => 100}},
-            :title_i => {2015 => 0.543},
-            :name => "academy 20"
-    }
-    ep = EconomicProfile.new(data)
+    ep = EconomicProfile.new(economic_data)
     assert_equal 50000, ep.median_household_income_in_year(2005)
     assert_equal 55000, ep.median_household_income_in_year(2009)
   end
 
   def test_raises_unknown_data_error_for_unknown_year
-    data = {:median_household_income => {[2005, 2009] => 50000, [2008, 2014] => 60000},
-            :children_in_poverty => {2012 => 0.1845},
-            :free_or_reduced_price_lunch => {2014 => {:percentage => 0.023, :total => 100}},
-            :title_i => {2015 => 0.543},
-            :name => "academy 20"
-    }
-    ep = EconomicProfile.new(data)
+    ep = EconomicProfile.new(economic_data)
     assert_raises UnknownDataError do
       ep.median_household_income_in_year(1999)
     end
   end
 
   def test_returns_median_household_income
-    data = {:median_household_income => {[2005, 2009] => 50000, [2008, 2014] => 60000},
-            :children_in_poverty => {2012 => 0.1845},
-            :free_or_reduced_price_lunch => {2014 => {:percentage => 0.023, :total => 100}},
-            :title_i => {2015 => 0.543},
-            :name => "academy 20"
-    }
-    ep = EconomicProfile.new(data)
+    ep = EconomicProfile.new(economic_data)
     assert_equal 55000, ep.median_household_income_average
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,3 +1,13 @@
 require "minitest"
 require 'simplecov'
 SimpleCov.start
+
+def economic_data
+  {
+    :median_household_income => {[2005, 2009] => 50000, [2008, 2014] => 60000},
+    :children_in_poverty => {2012 => 0.1845},
+    :free_or_reduced_price_lunch => {2014 => {:percentage => 0.023, :total => 100}},
+    :title_i => {2015 => 0.543},
+    :name => "academy 20"
+  }
+end
