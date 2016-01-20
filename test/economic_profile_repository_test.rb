5b961b50bdc0af1fdd4d56788da614b7c6ad2a50
Julian Feliciano <julsfelic@gmail.com>
2016-01-19 20:49:09 -0700

    Economic Profiles Repository can start off with an empty instance variable for economic profiles

--- test/economic_profile_repository_test.rb
+++ test/economic_profile_repository_test.rb
@@ -2,6 +2,11 @@ require "test_helper"
 require "economic_profile_repository"
 
 class EconomicProfileRepositoryTest < Minitest::Test
+  def test_starts_off_with_empty_economic_profiles
+    epr = EconomicProfileRepository.new
+    assert_equal [], epr.economic_profiles
+  end
+
   def test_can_find_economic_profile_by_name
     epr = EconomicProfileRepository.new
     epr.load_data(economic_profile_file)
