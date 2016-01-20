9c7385e6a28ebc05b76dbb907b6ca2514051b43e
Julian Feliciano <julsfelic@gmail.com>
2016-01-20 14:39:47 -0700

    Return the growth across all three subjects

--- test/headcount_analyst_test.rb
+++ test/headcount_analyst_test.rb
@@ -13,7 +13,6 @@ class HeadcountAnalystTest < Minitest::Test
     ha = HeadcountAnalyst.new(dr)
     rate = ha.kindergarten_participation_rate_variation("ACADEMY 20",
                                                         against: "COLORADO")
-
     assert_equal 0.766, rate
   end
 
@@ -21,40 +20,34 @@ class HeadcountAnalystTest < Minitest::Test
     ha = HeadcountAnalyst.new(dr)
     rate = ha.kindergarten_participation_rate_variation("ACADEMY 20",
                                                         against: "YUMA SCHOOL DISTRICT 1")
-
     assert_equal 0.447, rate
   end
 
-  def test_can_kindergarten_participation_trend_against_state_average
+  def test_can_get_kindergarten_participation_trend_against_state_average
     ha = HeadcountAnalyst.new(dr)
     trend = ha.kindergarten_participation_rate_variation_trend("ACADEMY 20",
                                                                against: "COLORADO")
-
     assert_instance_of Hash, trend
     refute trend[2010].nil?
   end
 
   def test_returns_rate_of_kindergarten_participation_versus_high_school_graduation_rate
     ha = HeadcountAnalyst.new(dr)
-
     assert_equal 0.642, ha.kindergarten_participation_against_high_school_graduation("ACADEMY 20")
   end
 
   def test_can_predict_correlation_of_kindergarten_participation_and_high_school_graduation_within_the_same_district
     ha = HeadcountAnalyst.new(dr)
-
     assert_equal true, ha.kindergarten_participation_correlates_with_high_school_graduation(for: "ACADEMY 20")
   end
 
   def test_can_predict_correlation_of_kindergarten_participation_and_high_school_graduation_across_the_state
     ha = HeadcountAnalyst.new(dr)
-
     assert_equal false, ha.kindergarten_participation_correlates_with_high_school_graduation(for: "STATEWIDE")
   end
 
   def test_can_predict_correlation_of_kindergarten_participation_and_high_school_graduation_across_different_districts
     ha = HeadcountAnalyst.new(dr)
-
     assert_equal false, ha.kindergarten_participation_correlates_with_high_school_graduation(across: ["ACADEMY 20", "CHERRY CREEK 5"])
   end
 
@@ -76,5 +69,12 @@ class HeadcountAnalystTest < Minitest::Test
     d = ha.top_statewide_test_year_over_year_growth(grade: 3, top: 3, subject: :math)
     expected = [["WILEY RE-13 JT", 0.3], ["SANGRE DE CRISTO RE-22J", 0.072], ["COTOPAXI RE-3", 0.07]]
     assert_equal expected, d
+  end
+
+  def test_returns_growth_across_all_three_subjects
+    ha = HeadcountAnalyst.new(dr)
+    result = ha.top_statewide_test_year_over_year_growth(grade: 3)
+    expected = ["SANGRE DE CRISTO RE-22J", 0.071]
+    assert_equal expected, result
   end
 end
